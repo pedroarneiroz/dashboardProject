7df39a8d3936ea6eba895653296d0ffd9520feed
pedroarneiroz <pedroarneiroz@gmail.com>
2023-03-07 22:35:38 -0300

    Add: ngOnInit and ngOnChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Chart } from 'chart.js'
+import { Component, Input, OnChanges, OnInit, ngDoCheck } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'menuProject';
+export class AppComponent implements OnInit, OnChanges, ngDoCheck {
+
+  @Input() public title: string = 'menuProject';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      alert("Teste ngOnInit with timeout");
+    }, 5000);
+  }
+
+ //Sempre é ativado quando recebe um dado de fora
+  ngOnChanges(): void {
+    console.log("Sempre é ativado quando recebe um dado de fora");
+  }
+
+    ngDoCheck(): void {}
+
 }
