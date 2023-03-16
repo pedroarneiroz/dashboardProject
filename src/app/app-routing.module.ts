9630a8a79794e561dd902f1a29e3976ad557e97b
pedroarneiroz <pedroarneiroz@gmail.com>
2023-03-15 21:21:05 -0300

    adding routes in the project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", component: DashboardComponent },
+  { path: "" }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule]
+})
+
+export class AppRoutingModule {}
